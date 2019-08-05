78284ac54f634099a63b9726f4dc8be89166b894
dhirajthakare <32159491+dhirajthakare@users.noreply.github.com>
2019-08-05 23:05:18 +0530

    first pattern

---
+++ patter1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namesapce std;
+int main(){
+int n,i,j;
+cout<<"Enter the pattern row size";
+cin>>n;
+for(i=0;i<n;i++) {
+cout<<endl;
+for(j=0;j<=i;j++)
+cout<<"* ";
+}
+return 0;
+}
+
+/*  pattern is
+  *
+  * *
+  * * *
+  * * * *
+  * * * * *
+  like that
+*/
